0b9115802525e3f277bcd173e826eccc3ae53ee9
VukMNE <vuk.djuranovic@udg.edu.me>
2021-05-04 18:08:08 +0200

    Fixed the bug, now it should work correctly

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def random_choice(cards):
     selected_card_index = random.choice(list(range(len(cards))))
     return selected_card_index
 
-def solve_move(talon, move, whose_move, my_cards, opp_cards, my_taken_cards, opp_taken_cards, my_points, opp_points):
+def solve_move(cards_on_table, move, whose_move, my_cards, opp_cards, my_taken_cards, opp_taken_cards, my_points, opp_points):
     # checks whether you take, or not
     # also, it checks whether it is a zing or not
     card_played = None
@@ -82,55 +82,57 @@ def solve_move(talon, move, whose_move, my_cards, opp_cards, my_taken_cards, opp
         card_played = opp_cards[move]
         opp_cards.pop(move)
 
-    if len(talon) == 0:
-        talon.append(card_played)
-    elif len(talon) == 1:
-        if talon[0].value != card_played.value:
-            talon.append(card_played)
+    if len(cards_on_table) == 0:
+        cards_on_table.append(card_played)
+    elif len(cards_on_table) == 1:
+        if cards_on_table[0].value != card_played.value:
+            cards_on_table.append(card_played)
             if card_played.value == 'J':
                 # print('debug 1')
-                collect_cards(talon, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points)
+                cards_on_table,my_taken_cards, opp_taken_cards, my_points, opp_points = collect_cards(cards_on_table, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points)
     else:
-        if (talon[-1].value == card_played.value) or (card_played.value == 'J'):
+        if (cards_on_table[-1].value == card_played.value) or (card_played.value == 'J'):
             # print('debug 2')
             # print(card_played.value)
             # print(str(card_played))
-            talon.append(card_played)
-            collect_cards(talon, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points)
+            cards_on_table.append(card_played)
+            cards_on_table,my_taken_cards, opp_taken_cards, my_points, opp_points = collect_cards(cards_on_table, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points)
         else:
-            talon.append(card_played)
+            cards_on_table.append(card_played)
+
+    return cards_on_table, my_cards, opp_cards, my_taken_cards, opp_taken_cards, my_points, opp_points
 
 
 
-def collect_cards(talon, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points):
+def collect_cards(cards_on_table, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points):
     print('COLLECTION HAPPENS')
-    print_cards_inline(talon)
-    if len(talon) == 2:
+    print_cards_inline(cards_on_table)
+    if len(cards_on_table) == 2:
         # check if it is a zing, check if both cards have the same value
-        if talon[0].value == talon[1].value:
+        if cards_on_table[0].value == cards_on_table[1].value:
             # it is a zing
             zing_points = 10
-            if talon[1].value == 'J':
+            if cards_on_table[1].value == 'J':
                 zing_points = 20
             if whose_move == 1:
                 my_points += zing_points
             else:
                 opp_points += zing_points
 
-    transfer_cards_and_points(talon, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points)
+    return transfer_cards_and_points(cards_on_table, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points)
 
-def transfer_cards_and_points(talon, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points):
+def transfer_cards_and_points(cards_on_table, whose_move, my_taken_cards, opp_taken_cards, my_points, opp_points):
     if whose_move == 1:
-        for i, t in enumerate(talon):
-            my_points += count_points_from(talon)
+        for i, t in enumerate(cards_on_table):
+            my_points += count_points_from(cards_on_table)
             my_taken_cards.append(t)
-            talon.pop(i)
     else:
-        for i, t in enumerate(talon):
-            opp_points += count_points_from(talon)
+        for i, t in enumerate(cards_on_table):
+            opp_points += count_points_from(cards_on_table)
             opp_taken_cards.append(t)
-            talon.pop(i)
 
+    cards_on_table = []
+    return cards_on_table,my_taken_cards, opp_taken_cards, my_points, opp_points
 
 def count_points_from(cards):
     pts = 0
@@ -147,7 +149,7 @@ if __name__ == '__main__':
 
     # Introducing some variables
     # deck is the shuffled list of cards with which we play
-    # talon is the table at which we trow cards
+    # cards_on_table is the table at which we trow cards
     # my_cards are cards that I have currently in hand
     # opponent_cards are cards that opponent has currently in hand
     # my_taken_cards are cards that I have taken (we count points from them)
@@ -159,7 +161,7 @@ if __name__ == '__main__':
 
 
     deck = shuffle_deck(create_deck())
-    talon = list()
+    cards_on_table = list()
     card_underneath = deck[-1]
 
     print('Deck is shuffled. Who plays first? If you go first, type in 1, if computer should go first press 2')
@@ -169,16 +171,13 @@ if __name__ == '__main__':
     my_taken_cards = list()
     opp_taken_cards = list()
 
-    talon = deck[:4]
+    cards_on_table = deck[:4]
     deck = deck[4:]
-    print('Cards on the talon are: ')
-    print_cards(talon)
+    print('Cards on the cards_on_table are: ')
+    print_cards(cards_on_table)
 
     my_cards, opponent_cards = deal_cards(who_is_first, deck)
-    print('-------------------------------------------------')
-    print('My Cards are')
-    print_cards_inline(my_cards)
-    print(len(deck))
+
     print('To play a card, press 1 for the first, 2 for second, etc....')
 
     round = 1
@@ -186,8 +185,11 @@ if __name__ == '__main__':
         while len(my_cards) + len(opponent_cards) > 0:
             if who_is_first == 1:
                 # -1 is because indexing goes from 0
+                print('--------------------IT IS YOUR MOVE NOW-----------------------------')
+                print('My Cards are')
+                print_cards_inline(my_cards)
                 my_move = int(input()) - 1
-                solve_move(talon, my_move, 1, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points)
+                cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points = solve_move(cards_on_table, my_move, 1, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points)
                 opp_move = opp_plays(opponent_cards)
                 print('**************************')
                 print('OPPONENT CARDS')
@@ -195,13 +197,14 @@ if __name__ == '__main__':
                 print('**************************')
                 print('opp move: ' + str(opp_move))
                 print('Oponent plays: ' + str(opponent_cards[opp_move]))
-                solve_move(talon, opp_move, 2, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points)
+                cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points = solve_move(cards_on_table, opp_move, 2, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points)
                 print('-------------------------------------------------')
-                print('Now the talon is:')
-                print_cards_inline(talon)
+                print('Now the cards_on_table is:')
+                print_cards_inline(cards_on_table)
 
                 print('++ Your cards are ++')
                 print_cards_inline(my_cards)
+        print('Deal the cards....')
         my_cards, opponent_cards = deal_cards(who_is_first, deck)
         round += 1
 
