a9eb46ee2fd9e32cf0142bbe0dcfa0fafd93819b
Marusa <orazemmarusa@gmail.com>
2021-05-26 14:29:34 +0200

    monte carlo should work

--- main.py
+++ main.py
@@ -1,5 +1,148 @@
 from utils import *
 from Heuristic import *
+from monte_carlo import *
+
+
+def play_monte_carlo_vs_random(who_is_first):
+    '''
+    This plays a game where one player randomly chooses cards and the other plays the best card according to heuristic function.
+    Player wins when he reaches at least 101 points.
+    :return: 1 if heuristic player wins, otherwise 0
+    '''
+
+    my_total_points = 0
+    opp_total_points = 0
+
+    while my_total_points < 101 and opp_total_points < 101:
+        deck = shuffle_deck(create_deck())
+        cards_on_table = list()
+        card_underneath = deck[-1]
+
+        if my_total_points > 0 or opp_total_points > 0:
+            # we need to switch who plays first everytime the deck gets empty
+            if who_is_first == 1:
+                who_is_first = 2
+            else:
+                who_is_first = 1
+        # print('Deck is shuffled. Who plays first? If you go first, type in 1, if computer should go first press 2')
+        # who_is_first = int(input())
+        # print('Your choice: ' + str(who_is_first))
+        my_points, opp_points = 0, 0
+        my_taken_cards = list()
+        opp_taken_cards = list()
+
+        cards_on_table = deck[:4]
+
+        # I put this code here, just to check whether we always shuffle the deck in the same manner
+        # if my_total_points == 0 and opp_total_points == 0:
+        #     print('First 4 are')
+        #     print_cards_inline(cards_on_table)
+
+        deck = deck[4:]
+
+
+        my_cards, opponent_cards, deck = deal_cards(who_is_first, deck)
+
+        # print('To play a card, press 1 for the first, 2 for second, etc....')
+
+        # we use this variable to see who is the last person who took the cards from the table
+        # this is important in order to solve to whom do the cards go at the end of the game
+        last_taken_by = 0
+
+        round = 1
+        while round <= 6:
+            # print('Cards on the cards_on_table are: ')
+            # print_cards_inline(cards_on_table)
+            while len(my_cards) + len(opponent_cards) > 0:
+                if who_is_first == 1:
+                    # heuristic agent moves first in this hand
+                    # print('--------------------IT IS YOUR MOVE NOW-----------------------------')
+                    # print('My Cards are')
+                    # print_cards_inline(my_cards)
+                    monte_carlo_card = monte_carlo_best_card(None, deck, cards_on_table, my_points, my_cards, my_total_points, my_taken_cards, opp_points, opponent_cards, opp_total_points, opp_taken_cards, who_is_first, last_taken_by)
+
+                    # print('Monte carlo:')
+                    # print(heuristic_score)
+                    my_move = monte_carlo_card
+                    prior_length = len(cards_on_table)
+                    # print('I play')
+                    # print(my_cards[my_move])
+                    cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points = solve_move(
+                        cards_on_table, my_move, 1, my_cards, opponent_cards, my_taken_cards, opp_taken_cards,
+                        my_points, opp_points, False)
+                    if prior_length > len(cards_on_table):
+                        last_taken_by = 1
+
+                    prior_length = len(cards_on_table)
+                    # code is moved to utils function opp_makes_a_move
+                    cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points = \
+                        opp_makes_a_move(cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards,
+                                         my_points, opp_points, show = False)
+                    if prior_length > len(cards_on_table):
+                        last_taken_by = 2
+                    # print('Cards on the cards_on_table are: ')
+                    # print_cards_inline(cards_on_table)
+
+                else:
+                    # computer (agent that makes random moves) moves first in this hand
+                    prior_length = len(cards_on_table)
+                    # code is moved to utils function opp_makes_a_move
+                    cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points = \
+                        opp_makes_a_move(cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards,
+                                         my_points, opp_points, show = False)
+                    if prior_length > len(cards_on_table):
+                        last_taken_by = 2
+                    # print('Cards on the cards_on_table are: ')
+                    # print_cards_inline(cards_on_table)
+                    #
+                    # print('--------------------IT IS YOUR MOVE NOW-----------------------------')
+                    # print('My Cards are')
+                    # print_cards_inline(my_cards)
+                    monte_carlo_card = monte_carlo_best_card(None, deck, cards_on_table, my_points, my_cards,
+                                                             my_total_points, my_taken_cards, opp_points, opponent_cards,
+                                                             opp_total_points, opp_taken_cards, who_is_first,
+                                                             last_taken_by)
+
+                    # print('Heuristic scores:')
+                    # print(heuristic_score)
+                    my_move = monte_carlo_card
+                    # print('I play')
+                    # print(my_cards[my_move])
+                    prior_length = len(cards_on_table)
+                    cards_on_table, my_cards, opponent_cards, my_taken_cards, opp_taken_cards, my_points, opp_points = solve_move(
+                        cards_on_table, my_move, 1, my_cards, opponent_cards, my_taken_cards, opp_taken_cards,
+                        my_points, opp_points, False)
+                    if prior_length > len(cards_on_table):
+                        last_taken_by = 1
+
+            # print('Deal the cards....')
+            my_cards, opponent_cards, deck = deal_cards(who_is_first, deck)
+            # print('Deck size: ' + str(len(deck)))
+            round += 1
+
+        # What remains after the last hand goes to the player who last took the cards in the game
+        if len(cards_on_table) > 0:
+            cards_on_table, my_taken_cards, opp_taken_cards, my_points, opp_points = transfer_cards_and_points(
+                cards_on_table, last_taken_by, my_taken_cards, opp_taken_cards, my_points, opp_points)
+
+        if len(my_taken_cards) > len(opp_taken_cards):
+            my_points += 3
+        elif len(my_taken_cards) < len(opp_taken_cards):
+            opp_points += 3
+        # print('++++++++++++++++++++++++ ROUND FINISHED ++++++++++++++++')
+        # print('Your points: ' + str(my_points))
+        # print('Opponent points: ' + str(opp_points))
+        my_total_points = my_total_points + my_points
+        opp_total_points = opp_total_points + opp_points
+
+
+    # print('Game finished')
+    # print('Heuristic points: ' + str(my_total_points))
+    # print('Random agent points: ' + str(opp_total_points))
+    # print('------------------------')
+    if my_total_points > opp_total_points:
+        return 1
+    return 0
 
 
 def play_random_vs_heuristic(who_is_first):
@@ -412,7 +555,7 @@ if __name__ == '__main__':
     n = 10000
     for _ in range(n):
         plays_first = (_ % 2) + 1
-        wins += play_h1_vs_h2(plays_first)
+        wins += play_monte_carlo_vs_random(plays_first)
     print(f"Wins: {wins/n}")
 
 
